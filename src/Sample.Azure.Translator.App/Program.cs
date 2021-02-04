2e322e3834f1fa5d62282d5fac87b6168e609f3f
Pon Cheol, Ku (구본철) <bbon@bbon.kr>
2021-02-04 18:36:08 +0900

    Add  file load service

--- src/Sample.Azure.Translator.App/Program.cs
+++ src/Sample.Azure.Translator.App/Program.cs
@@ -8,30 +8,82 @@ using Microsoft.Extensions.Configuration;
 using System.Text.Json;
 using Microsoft.Extensions.Options;
 using System.IO;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using Sample.Azure.Translator.App.Models;
+using Sample.Azure.Translator.App.Services;
 
 namespace Sample.Azure.Translator.App
 {
     class Program
     {
-        public static Task Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            using var host = CreateHostBuilder(args).Build();
+            var host = CreateHostBuilder(args).Build();
 
-            RunProcess(host);
+            await RunProcess(host, args).ConfigureAwait(true);
 
-            return host.RunAsync();
+            //return host.RunAsync();
         }
 
-        private static Task RunProcess(IHost host)
+        private static async Task RunProcess(IHost host, string[] args)
         {
-            // code here
+            var text = string.Empty;
 
-            return Task.CompletedTask;
+            // code here
+            if(args.Length > 0)
+            {
+                var localFileService = host.Services.GetRequiredService<ILocalFileService>();
+
+                if (localFileService.Exists(args[0]))
+                {
+                    text = await localFileService.ReadAsync(args[0]);
+                }
+            }
+
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                Console.WriteLine("Please type to want translation then press a return key (or enter key).");
+                Console.WriteLine("If want to exit this app, press 'ctrl' + 'c'.");
+                do
+                {
+                    Console.WriteLine("Input text:");
+                    text = Console.ReadLine();
+                }
+
+                while (string.IsNullOrWhiteSpace(text));
+            }
+
+            var translatorService = host.Services.GetRequiredService<ITranslatorService>();
+
+            var source = new TranslationRequestModel
+            {
+                Inputs = new TranslationRequestInputModel[]
+                {
+                    new TranslationRequestInputModel(text),
+                },
+                TranslateToLanguages = new string[] { "en", "ru", "ja" },
+            };
+
+            try
+            {
+                var result = await translatorService.TranslateAsync(source);
+
+                Console.WriteLine($"Result: {result.ToJson()}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+         
         }
 
         private static IServiceCollection ConfigureServices(IServiceCollection services)
         {
             // DI here
+            services.AddTransient<ILocalFileService, LocalFileService>();
+            services.AddTransient<ITranslatorService, TranslatorService>();
 
             return services;
         }
@@ -83,7 +135,10 @@ namespace Sample.Azure.Translator.App
     {
         public static string ToJson<T>(this T obj, JsonSerializerOptions options = null)
         {
-            var actualOptions = options ?? new JsonSerializerOptions { WriteIndented = true };
+            var actualOptions = options ?? new JsonSerializerOptions { 
+                WriteIndented = true,
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.All, UnicodeRanges.Cyrillic),
+            };
 
             return JsonSerializer.Serialize<T>(obj, actualOptions);
         }
---
+++ src/Sample.Azure.Translator.App/Services/LocalFileService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+namespace Sample.Azure.Translator.App.Services
+{
+    public interface ILocalFileService
+    {
+        Task<string> ReadAsync(string filePath);
+        
+        bool Exists(string filePath);
+    }
+    public class LocalFileService : ILocalFileService
+    {
+        public LocalFileService(ILoggerFactory loggerFactory)
+        {
+            logger = loggerFactory.CreateLogger<LocalFileService>();
+        }
+
+        public Task<string> ReadAsync(string filePath)
+        {
+            if (!Exists(filePath))
+            {
+                throw new FileNotFoundException($"File does not find. ({filePath})");
+            }
+
+            return File.ReadAllTextAsync(filePath);
+        }
+
+        public bool Exists(string filePath) => File.Exists(filePath);
+
+        private ILogger logger;
+    }
+}
